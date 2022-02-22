68c14973cfb777774712e06ae676d069abdb4a2a
Mox1k <83392087+Mox1k@users.noreply.github.com>
2022-02-22 09:12:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LINQ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill" };
+
+            // создаем новый список для результатов
+            var selectedPeople = new List<string>();
+            // проходим по массиву
+            foreach (string person in people)
+            {
+                // если строка начинается на букву T, добавляем в список
+                if (person.ToUpper().StartsWith("T"))
+                    selectedPeople.Add(person);
+            }
+            // сортируем список
+            selectedPeople.Sort();
+
+            foreach (string person in selectedPeople)
+
+            Console.WriteLine(person);
+        }
+    }
+}
